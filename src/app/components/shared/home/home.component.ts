1f997ad43514bf0c0aa0d431300be7afedf28426
victorpreston <prestonvictor25@gmail.com>
2024-11-04 18:37:20 +0300

    zendp: home component i18n

--- src/app/components/shared/home/home.component.ts
+++ src/app/components/shared/home/home.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from "../footer/footer.component";
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, CommonModule, NavbarComponent, FooterComponent],
+  imports: [RouterLink, CommonModule, NavbarComponent, FooterComponent, TranslateModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -17,26 +18,10 @@ export class HomeComponent {
   selectedMethod = 'Select method';
   currentSlide = 0;
   faqs = [
-    {
-      question: 'How long does a transfer take?',
-      answer: 'Typically, transfers take between 1-2 business days, but some methods are instant depending on the receiving bank and country.',
-      isOpen: false,
-    },
-    {
-      question: 'What are the fees for sending money?',
-      answer: 'Our fees vary depending on the transfer amount, destination, and payment method. We offer competitive rates to ensure you get the most value.',
-      isOpen: false,
-    },
-    {
-      question: 'Is it safe to send money with ZendP?',
-      answer: 'Yes, we use advanced encryption and security protocols to keep your transactions and personal information safe and secure.',
-      isOpen: false,
-    },
-    {
-      question: 'Can I track my transfer?',
-      answer: 'Yes, you can track your transfer in real-time through our app or website by entering your transaction ID.',
-      isOpen: false,
-    }
+    { isOpen: false },
+    { isOpen: false },
+    { isOpen: false },
+    { isOpen: false }
   ];
 
    toggleFaq(index: number) {
